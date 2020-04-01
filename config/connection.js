a6103ef009b4253af8ec23ffbf3a347b7ee3e65f
Marc Dycus <marcdycus@yahoo.com>
2020-04-01 15:40:33 -0400

    server

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,12 @@ var mysql = require("mysql");
 
 var connection;
 
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        // host: "localhost",
+        host: "localhost",
         socketPath: '/tmp/mysql.sock',
         port: 3306,
         user: "root",
@@ -23,4 +24,12 @@ connection.connect(function(err) {
     console.log("connected as id: " + connection.threadId);
 });
 
+var del = connection._protocol._delegateError;
+
+connection._protocol._delegateError = function(err, sequence){
+  if (err.fatal) {
+    console.trace('fatal error: ' + err.message);
+  }
+  return del.call(this, err, sequence);
+};
 module.exports = connection;
