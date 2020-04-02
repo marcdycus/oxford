27802b6d93bfa35f58c29a902a765c09d4c37f03
Marc Dycus <marcdycus@yahoo.com>
2020-04-01 23:07:40 -0400

    styles

--- config/connection.js
+++ config/connection.js
@@ -8,12 +8,11 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
         host: "localhost",
-        socketPath : '/tmp/mysqld.sock',
         port: 3306,
         user: "root",
         password: "password",
         database: "waitlist_db"
-        });
+    });
 }
 
 connection.connect(function(err) {
@@ -22,14 +21,7 @@ connection.connect(function(err) {
         return;
     } 
     console.log("connected as id: " + connection.threadId);
+
 });
 
-// var del = connection._protocol._delegateError;
-
-// connection._protocol._delegateError = function(err, sequence){
-//   if (err.fatal) {
-//     console.trace('fatal error: ' + err.message);
-//   }
-//   return del.call(this, err, sequence);
-// };
 module.exports = connection;
